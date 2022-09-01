1fc13447a1d61e0cd327cdd4f445ba64bb8ff35e
Jonathas Veras <jonathasveras42@gmail.com>
2022-09-01 11:01:32 -0300

    tirando cout de variance

--- exercicio_aula2/variance_question/arithmetic_mean.cpp
+++ exercicio_aula2/variance_question/arithmetic_mean.cpp
@@ -1,17 +1,12 @@
 #include "arithmetic_mean.h"
 #include <stdlib.h>
-#include <iostream>
-using std::cout;
 
 float arithmeticMean(int total_elements, char *argv[]){
     float total = 0;
-    cout << "total elements:" << total_elements << std::endl;
     for (int i = 0; i < total_elements; i++)
     {
-        cout << "element:" << atoi(argv[i]) << std::endl;
         total += atoi(argv[i]);
     }
-    cout << "arithmetic value: " <<((total) / (total_elements)) << std::endl;
     return ((total) / (total_elements));
 }
 
